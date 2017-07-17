42bacf18e49f4a1d32eb62b4458f1bc8cb1d9542
Carlos Cinelli <carloscinelli@hotmail.com>
2017-07-17 13:19:36 -0700

    darfur data

---
+++ R/darfur_data.R
@@ -0,0 +1,11 @@
+#### Darfur Data ####
+
+##' Darfur Data
+##'
+##' Chad's data for Darfur paper
+##'
+##' @docType data
+##' @keywords dataset
+##' @format A data frame with xx rows and xx variables
+##' @name darfur
+NULL
