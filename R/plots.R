675985439fb62f8d4118c6984a8b27df94c66b47
Carlos Cinelli <carloscinelli@hotmail.com>
2017-07-17 18:19:39 -0700

    base?

--- R/plots.R
+++ R/plots.R
@@ -17,9 +17,25 @@ plot.sensemade <- function(x, type = "xxx", ...){
   )
 }
 
-contourplot <- function(...){
-  #
+contourplot <- function(x, type = "estimate", nlevels = 15, pch = 20, cex = 0.5, lim = NULL,
+                         xlab = "Hypothetical partial R2 of unobserved confounder with the treatment",
+                         ylab = "Hypothetical partial R2 of unobserved confounder withthe outcome",
+                         main = "Sensitivity of estimate to unobserved confounder\nContours of adjusted estimates",
+                         ...){
+  r2y <- x$benchmarks$r2y
+  r2d <- x$benchmarks$r2d
+  estimate <- x$sensitivity$estimate
+  k <- x$sensitivity$se * sqrt(x$sensitivity$dof)
 
+  if (is.null(lim)) lim <- max(c(r2y, r2d), na.rm = TRUE) + 0.1
+  s <- seq(0, lim, by = 0.001)
+  z <- estimate - outer(s, s, biasR2, k = k)
+
+  contour(s, s, z, nlevels = nlevels,
+          xlab = xlab,
+          ylab = ylab,
+          main = main, ...)
+  points(r2d, r2y, pch = pch, cex = cex)
 }
 
 
