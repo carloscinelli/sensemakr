2b1cd75d9bf1b754db4e6e363622e4a612a4185a
Carlos Cinelli <carloscinelli@hotmail.com>
2017-07-18 12:30:21 -0700

    all

--- R/sensemakr.R
+++ R/sensemakr.R
@@ -3,7 +3,7 @@
 ##' @description Description.
 ##'
 ##' @param D  character vector with the treatment variable.
-##' @param group_list  character vector with the covariates for benchmarking.
+##' @param X  character vector with the covariates for benchmarking.
 ##' @param ... extra arguments
 ##' @return The function returns an object of class 'sensemakr' which is a list with the main
 ##' results for sensitivity analysis, namely:
@@ -31,7 +31,7 @@
 ##' Cite paper(s)
 ##'
 ##' @export
-sensemakr <- function(...){
+sensemakr <- function(model, D, X=NULL,...){
   UseMethod("sensemakr")
 }
 
@@ -39,16 +39,19 @@ sensemakr <- function(...){
 ##' @param model the model.
 ##' @name sensemakr
 ##' @export
-sensemakr.lm <- function(model, D, group_list = NULL, ...){
+sensemakr.lm <- function(model, D, X=NULL, ...){
   # stats <- get stats()
   # benchmarks <- get benchmarks()
   # compute bias and include ob data.frames
   # returns pretty list with class "sensemade"
+
   treat.stats <- getstats(model, D)
-  benchmarks  <- benchmarkr(model, D, group_list)
+  benchmarks  <- benchmarkr(model, D, X)
   out <- list(treat.stats = treat.stats,
               benchmarks = benchmarks,
-              model = model)
+              info = list(outcome = deparse(model$terms[1][[2]]),
+                          treatment = D,
+                          model = model))
   class(out) <- "sensemade"
   return(out)
 }
@@ -78,7 +81,7 @@ getstats <- function(model, D){
 #                  -> if a list of character vectors, then group by list (future work)
 # Output: three data.frames with benchmarks for R2, SD and natural.
 #       - data.frame contains: Names, R2y or delta, R2d or gamma
-benchmarkr <- function(model, D, group_list = NULL, ...){
+benchmarkr <- function(model, D, X = NULL, ...){
 
   treat.stats  <- getstats(model, D)
   estimate     <- treat.stats$estimate
@@ -87,17 +90,9 @@ benchmarkr <- function(model, D, group_list = NULL, ...){
   summ.out     <- summary(model)
   coef.out     <- coef(summ.out)
 
-  # if(is.null(X)){
-    # coef.out=coef(summary(lm(data=iris,Sepal.Width~Sepal.Length)))
-    # X is character vector of term names (non intercept, non treatment)
-    X <- rownames(coef.out)[!(rownames(coef.out) %in% c("(Intercept)",D))]
-    # carlos prototype X used as basic row name index of model matrix cols
-
-    # the way X is used, I don't think we need X here
-    # mike anticipated X to be user way to spec list of generic sets of terms
-    # to be used as list of simultaneous vars
-    # X = list('village',c('female','village'),'village')
-  # }
+  if (is.null(X)) {
+    X <- rownames(coef.out)[!rownames(coef.out) %in% c("(Intercept)",D)]
+  }
 
   tstats.out        <- coef.out[X, "t value"]
   r2y       <- tstats.out^2/(tstats.out^2 + df.out) # partial R2 with outcome
@@ -114,8 +109,7 @@ benchmarkr <- function(model, D, group_list = NULL, ...){
 
   ## R2d: gets t-statistics from treatment regression
   coef.treat <- coef(summ.treat)
-  pick       <- rownames(coef.treat)[!rownames(coef.treat) %in% "(Intercept)"]
-  tstats.treat        <- coef.treat[pick, "t value"] # excludes intercept
+  tstats.treat  <- coef.treat[X, "t value"] # excludes intercept
   r2d       <- tstats.treat^2/(tstats.treat^2 + df.treat) # partial R2 with treatment
 
 
@@ -129,28 +123,44 @@ benchmarkr <- function(model, D, group_list = NULL, ...){
 
   ## standardized coefficients
   sdy       <- sd(model$model[[1]]) # sd of outcome
-  Xn         <- model.matrix(model)
-  sdd       <- sd(Xn[,2]) # sd of treatment
-  sdx       <- apply(Xn[,-c(1:2)], 2, sd) # sd of covariates
+  Xn        <- model.matrix(model)[,c(D,X)]
+  sdd       <- sd(Xn[,D]) # sd of treatment
+  sdx       <- apply(Xn[,X], 2, sd) # sd of covariates
+  estimate_std <- estimate*(sdd/sdy)
   imp_std   <- impact*(sdx/sdy)
   imb_std   <- imbalance*(sdd/sdx)
 
   # space for groups R2
   # compute groups R2 and bind on r2y r2d
+  # worst case scenario benchmark
+  allvars <- rownames(coef.out)[!rownames(coef.out) %in% D]
+  r2y_all <- groupR2(model, allvars)
+  r2d_all <- groupR2(treat, allvars)
+  bias_all <- getbiasR2(sed, df.out, r2y_all, r2d_all)
 
   # biases
   bias_r2 = getbiasR2(sed, df.out, r2y, r2d)
   bias_nat = impact*imbalance
   bias_std = imp_std*imb_std
 
+  benchmark_all_vars <- data.frame(r2y_all = r2y_all,
+                                  r2d_all = r2d_all,
+                                  adj_est_all = adjust_estimate(estimate, bias_all),
+                                  adj_se_r2 = getseR2(sed, df.out, r2y_all, r2d_all),
+                                  adj_t_r2 = gettR2(estimate/sed, df.out, r2y_all, r2d_all),
+                                  row.names = NULL,
+                                  stringsAsFactors = FALSE)
 
   benchmark_R2  <- data.frame(covariate = X,
                               r2y = r2y,
                               r2d = r2d,
                               bias_r2 = bias_r2,
                               adj_est_r2 = adjust_estimate(estimate, bias_r2),
+                              adj_se_r2 = getseR2(sed, df.out, r2y, r2d),
+                              adj_t_r2 = gettR2(estimate/sed, df.out, r2y, r2d),
                               row.names = NULL,
                               stringsAsFactors = FALSE)
+  benchmark_R2 <- benchmark_R2[order(benchmark_R2$bias_r2, decreasing = TRUE), ]
 
   benchmark_natural <- data.frame(covariate = X,
                                   impact = impact,
@@ -159,89 +169,19 @@ benchmarkr <- function(model, D, group_list = NULL, ...){
                                   adj_est_nat = adjust_estimate(estimate, bias_nat),
                                   row.names = NULL,
                                   stringsAsFactors = FALSE)
+  benchmark_natural <- benchmark_natural[order(benchmark_natural$bias_nat, decreasing = TRUE), ]
 
   benchmark_std <- data.frame(covariate = X,
                               impact_std = imp_std,
                               imbalance_std = imb_std,
                               bias_std = bias_std,
-                              adj_est_std = adjust_estimate(estimate, bias_std),
+                              adj_est_std = adjust_estimate(estimate_std, bias_std),
                               row.names = NULL,
                               stringsAsFactors = FALSE)
+  benchmark_std <- benchmark_std[order(benchmark_std$bias_std, decreasing = TRUE), ]
 
-  # any 'blacklisted' terms (outcome model) that should be grouped?
-  # ?class_df_from_term
-  class_df = class_df_from_term(model)
-  blacklist_4_group = c('factor','matrix','smooth')
-  list_term_in_blacklist = lapply(X=class_df,FUN=function(x){x %in% blacklist_4_group})
-  terms_in_blacklist = names(which(unlist(list_term_in_blacklist)))
-
-
-  ############################################
-  # ?groupR2
-  # lapply cycle over 'group_list'
-  # groupR2(model=model)
-  # groupR2(model=treat)
-  ############################################
-  # group_list = list('village',c('female','village'),'village')
-
-  if(!is.null(group_list)){
-    # user spec list of group terms + all RHS terms in group blacklist
-    terms_force_group = append(group_list,terms_in_blacklist)
-    # c(NULL,'a')
-    # c(NULL,NULL)
-  }else{
-    terms_force_group = terms_in_blacklist
-    # terms_force_group = NULL
-  }
-
-  if(!is.null(terms_force_group)){
-
-    # terms_force_group = c('village','female')
-
-    r2y_combinevar = lapply(X=terms_force_group,
-                            FUN=function(X){
-                              groupR2(model=model,terms_4_group=X)
-                            })
-
-    names(r2y_combinevar) = unlist(lapply(terms_force_group,
-                                          FUN=function(X){
-                                            paste0(paste(X,collapse=","))
-                                          }))
-
-
-    r2d_combinevar = lapply(X=terms_force_group,
-                            FUN=function(X){
-                              groupR2(model=treat,terms_4_group=X)
-                            })
-
-    names(r2d_combinevar) = unlist(lapply(terms_force_group,
-                                          FUN=function(X){
-                                            paste0(paste(X,collapse=","))
-                                          }))
-
-    # bias from R2 param,
-    # consult if this applies to groupR2
-    r2pairs = cbind(r2y=r2y_combinevar,r2d=r2d_combinevar)
-
-    bias_r2_combinevar = apply(X=r2pairs,MARGIN=1,
-                               FUN=function(X){
-                                 getbiasR2(sed, df.out, r2y=X$r2y, r2d=X$r2d)
-                                 })
-
-    benchmark_R2_group  <- data.frame(covariate = names(r2y_combinevar),
-                                      r2y = unlist(r2y_combinevar),
-                                      r2d = unlist(r2d_combinevar),
-                                      bias_r2 = bias_r2_combinevar,  # consult
-                                      adj_est_r2 = adjust_estimate(estimate, bias_r2_combinevar),  # consult
-                                      row.names = NULL,
-                                      stringsAsFactors = FALSE)
-  }else{
-    benchmark_R2_group = NULL
-  }
-
-
-  benchmarks <- list(benchmark_R2 = benchmark_R2,
-                     benchmark_R2_group = benchmark_R2_group,
+  benchmarks <- list(benchmark_all_vars = benchmark_all_vars,
+                     benchmark_R2 = benchmark_R2,
                      benchmark_natural = benchmark_natural,
                      benchmark_std = benchmark_std)
 
@@ -249,28 +189,52 @@ benchmarkr <- function(model, D, group_list = NULL, ...){
 
 }
 
-##' @title Computes  bias caused by unobserved confounder
-##' @description  This function computes the bias caused by an unobserved confounder
+
+groupR2 <- function(model, coefs){
+
+  # betas
+  betas <- coef(model)
+
+  # var-covar matrix
+  V <- vcov(model)
+
+  # degrees of freedom
+  df <- df.residual(model)
+
+  # coefs to group
+  indx <- names(betas) %in% coefs
+
+  if (length(indx) == 0) {
+    warning(paste("No covariates found matching the word:", coefs, "\nNA returned"))
+    return(NA)
+  }
+
+  # compute F and R2
+  q <- length(indx)
+  f <- (t(betas[indx]) %*% solve(V[indx, indx]) %*% betas[indx])/q
+  f*q / (f*q + df)
+}
+
+##' @title Computes effects on estimate, standard error and t-value caused by unobserved confounder
+##' @description  These functions compute the bias caused by an unobserved confounder with a specific pair
+##' of partial R2 with the treatment and with the outcome.
 ##' with certain characteristics.
 ##'
 ##' @param se       standard error of original  treatment effect estimate
 ##' @param df       degrees of freedom of the original linear model
 ##' @param r2d      hypothetical partial R2 of the confounder with the treatment
 ##' @param r2y      hypothetical partial R2 of the confounder with the outcome
-getbiasR2 <- function(se, df, r2d, r2y, ...){
-  bias <- sqrt(r2y*r2d/(1 - r2d))*se*sqrt(df)
-  return(bias)
-}
+##' @param ...      extra arguments
+##' @export
+getbiasR2 <- function(se, df, r2d, r2y, ...) bias <- sqrt(r2y*r2d/(1 - r2d))*se*sqrt(df)
 
-adjust_estimate <- function(estimate, bias){
-  sign(estimate)*(abs(estimate) - bias)
-  }
+##' @export
+##' @name getbiasR2
+getseR2   <- function(se, df, r2y, r2d) sqrt((1 - r2y)/(1 - r2d))*se*sqrt(df/(df - 1))
+
+##' @export
+##' @name getbiasR2
+gettR2    <- function(t,df, r2y, r2d)  (t/sqrt(df) - sqrt(r2d*(r2y/(1 - r2y))))*sqrt((1 - r2d)/(1 - r2y))*sqrt(df - 1)
 
 
-# getbiasR2.lm(model, D, r2d, r2y){
-#
-# }
-
-# getbiasR2.sensemade()
-
-# mike test push pull
+adjust_estimate <- function(estimate, bias) sign(estimate)*(abs(estimate) - bias)
