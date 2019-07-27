10e3728c26b06ed008239048e94a4dda9642058c
Carlos Cinelli <carloscinelli@hotmail.com>
2019-07-26 21:02:35 -0700

    Mention paper onAttach

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onAttach <-
+  function(libname, pkgname) {
+    packageStartupMessage("See details in:")
+    packageStartupMessage("Carlos Cinelli and Chad Hazlett (2018). Making Sense of Sensitivity: Extending Omitted Variable Bias.")
+  }
+
