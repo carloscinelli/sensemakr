2cf8058585300f1eb4d01e865e20ef5880355027
grlju <grigorij.ljubownikow@gmail.com>
2022-07-27 19:00:17 +1200

    fixest formula method

--- R/sensemakr.R
+++ R/sensemakr.R
@@ -112,11 +112,9 @@ NULL
 #' the functions for computing the robustness value and partial R2 (\code{\link{robustness_value}}, \code{\link{partial_r2}}),  or the functions for bounding the strength
 #' of unobserved confounders (\code{\link{ovb_bounds}}), among others.
 #'
-#' @param ... arguments passed to other methods. First argument should either be (i) an \code{\link{lm}} model with the
+#' @param ... arguments passed to other methods. First argument should either be (i) an \code{\link{lm}} or \code{\link{fixest::feols}} model with the
 #' outcome regression (argument \code{model}); (ii) a \code{\link{formula}} describing the model along
 #' with the \code{\link{data.frame}} containing the variables of the model (arguments \code{formula} and \code{data}); or, (iii) the numerical estimated value of the coefficient, along with the numeric values of standard errors and degrees of freedom (arguments \code{estimate}, \code{se} and \code{df}).
-#'
-#'
 #'
 #'
 #' @references Cinelli, C. and Hazlett, C. (2020), "Making Sense of Sensitivity: Extending Omitted Variable Bias." Journal of the Royal Statistical Society, Series B (Statistical Methodology).
@@ -362,10 +360,12 @@ sensemakr.fixest <- function(model,
 
 
 #' @param formula an object of the class \code{\link{formula}}: a symbolic description of the model to be fitted.
+#' @param method the default is \code{\link{lm}}. This argument can be changed to estimate the model using \code{\link{fixest::feols}}. In this case the formula needs to be written so it can be estimated with \code{\link{fixest::feols}} and the package need to be installed.
 #' @param data data needed only when you pass a formula as first parameter. An object of the class \code{\link{data.frame}} containing the variables used in the analysis.
 #' @rdname sensemakr
 #' @export
-sensemakr.formula_lm <- function(formula,
+sensemakr.formula <- function(formula,
+                              method = c("lm", "feols"),
                               data,
                               treatment,
                               benchmark_covariates = NULL,
@@ -385,9 +385,18 @@ sensemakr.formula_lm <- function(formula,
   check_multipliers(ky = ky,
                     kd = kd)
 
+  type <- match.arg(method, method)
 
-  lm.call <- call("lm", formula = substitute(formula), data = substitute(data))
-  outcome_model = eval(lm.call)
+  if(type == "lm") {
+    reg.call <- call(type, formula = substitute(formula), data = substitute(data))
+    outcome_model = eval(reg.call)
+  } else if(type == "feols") {
+    if (!require("fixest")) {
+      stop("Please install the fixest package.")
+    }
+    reg.call <- call(type, fml = substitute(formula), data = substitute(data))
+    outcome_model = eval(reg.call)
+  }
 
   sensemakr(model = outcome_model,
             treatment = treatment,
